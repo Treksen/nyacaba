d38be783650d2a59454c80f6c653d6287c809071
Treksen <namsaoffice01@gmail.com>
2026-06-25 15:25:55 +0300

    App Update Prompt

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import { APP_VERSION } from './src/lib/constants';
 
 export default defineConfig({
   plugins: [
